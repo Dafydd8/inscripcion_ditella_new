adbb64326eacbbfd147802e3047cfc412e0c4285
Dafydd Jenkins <dafydd.a.jenkins@gmail.com>
2025-07-26 14:13:52 -0300

    lil fix

--- main.py
+++ main.py
@@ -68,13 +68,12 @@ except:
 WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div/div/div[2]/div[4]/div/div[1]/h2[2]'))).click()
 print("Se hizo click en el botón de materias fijadas.")
 
-# Obtener las tarjetas de materias
-cards =driver.find_elements(By.CLASS_NAME, 'desktop-subject-item-container')
-print(cards)
-
 # Esperar hasta que sea la hora de inscripción
 while esperar and datetime.now() < hora_de_inscripcion:
 	pass
+
+# Obtener las tarjetas de materias
+cards =driver.find_elements(By.CLASS_NAME, 'desktop-subject-item-container')
 
 # Inscribir en las materias
 for i, card in enumerate(cards):
